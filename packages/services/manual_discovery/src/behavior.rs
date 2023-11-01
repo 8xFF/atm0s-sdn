a8ba95c0a2b977ab6208dc5c4b0bd56d8521a31b
Giang Minh <giangndm@ows.vn>
2023-11-01 17:02:43 +0700

    migrate manual-discovery

--- packages/services/manual_discovery/src/behavior.rs
+++ packages/services/manual_discovery/src/behavior.rs
@@ -2,10 +2,13 @@ use crate::handler::ManualHandler;
 use crate::msg::*;
 use crate::MANUAL_SERVICE_ID;
 use bluesea_identity::{ConnId, NodeAddr, NodeAddrType, NodeId};
+use network::behaviour::BehaviorContext;
+use network::behaviour::NetworkBehaviorAction;
 use network::behaviour::{ConnectionHandler, NetworkBehavior};
+use network::transport::TransportOutgoingLocalUuid;
 use network::transport::{ConnectionRejectReason, ConnectionSender, OutgoingConnectionError};
-use network::BehaviorAgent;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::sync::Arc;
 use utils::Timer;
 
@@ -14,7 +17,7 @@ const CONNECT_WAIT_MAX: u64 = 30000;
 
 enum OutgoingState {
     New,
-    Connecting(u64, ConnId, usize),
+    Connecting(u64, Option<ConnId>, usize),
     Connected(u64, ConnId),
     ConnectError(u64, Option<ConnId>, OutgoingConnectionError, usize),
 }
@@ -31,13 +34,14 @@ pub struct ManualBehaviorConf {
     pub timer: Arc<dyn Timer>,
 }
 
-pub struct ManualBehavior {
+pub struct ManualBehavior<HE> {
     node_id: NodeId,
     neighbours: HashMap<NodeId, NodeSlot>,
     timer: Arc<dyn Timer>,
+    queue_action: VecDeque<NetworkBehaviorAction<HE>>,
 }
 
-impl ManualBehavior {
+impl<HE> ManualBehavior<HE> {
     pub fn new(conf: ManualBehaviorConf) -> Self {
         let mut neighbours = HashMap::new();
         for addr in conf.neighbours {
@@ -58,11 +62,12 @@ impl ManualBehavior {
             node_id: conf.node_id,
             neighbours,
             timer: conf.timer,
+            queue_action: VecDeque::new(),
         }
     }
 }
 
-impl<BE, HE> NetworkBehavior<BE, HE> for ManualBehavior
+impl<BE, HE> NetworkBehavior<BE, HE> for ManualBehavior<HE>
 where
     BE: From<ManualBehaviorEvent> + TryInto<ManualBehaviorEvent> + Send + Sync + 'static,
     HE: From<ManualHandlerEvent> + TryInto<ManualHandlerEvent> + Send + Sync + 'static,
@@ -71,34 +76,20 @@ where
         MANUAL_SERVICE_ID
     }
 
-    fn on_tick(&mut self, agent: &BehaviorAgent<BE, HE>, ts_ms: u64, _interal_ms: u64) {
+    fn on_tick(&mut self, context: &BehaviorContext, now_ms: u64, _interal_ms: u64) {
         for (node_id, slot) in &mut self.neighbours {
             if slot.incoming.is_none() {
                 match &slot.outgoing {
-                    OutgoingState::New => match agent.connect_to(*node_id, slot.addr.clone()) {
-                        Ok(conn) => {
-                            log::info!("[ManualBehavior {}] connect to {} with addr {} => conn: {}", self.node_id, node_id, slot.addr, conn.conn_id);
-                            slot.outgoing = OutgoingState::Connecting(ts_ms, conn.conn_id, 0);
-                        }
-                        Err(err) => {
-                            log::error!("[ManualBehavior {}] connect to {} with addr {} => error {:?}", self.node_id, node_id, slot.addr, err);
-                            slot.outgoing = OutgoingState::ConnectError(ts_ms, None, err, 0);
-                        }
-                    },
+                    OutgoingState::New => {
+                        self.queue_action.push_back(NetworkBehaviorAction::ConnectTo(0, *node_id, slot.addr.clone()));
+                        slot.outgoing = OutgoingState::Connecting(now_ms, None, 0);
+                    }
                     OutgoingState::ConnectError(ts, _conn, _err, count) => {
                         let sleep_ms = CONNECT_WAIT.get(*count).unwrap_or(&CONNECT_WAIT_MAX);
-                        if ts + *sleep_ms < ts_ms {
+                        if ts + *sleep_ms < now_ms {
                             //need reconnect
-                            match agent.connect_to(*node_id, slot.addr.clone()) {
-                                Ok(conn) => {
-                                    log::info!("[ManualBehavior {}] reconnect to {} with addr {} => conn: {}", self.node_id, node_id, slot.addr, conn.conn_id);
-                                    slot.outgoing = OutgoingState::Connecting(ts_ms, conn.conn_id, count + 1);
-                                }
-                                Err(err) => {
-                                    log::error!("[ManualBehavior {}] reconnect to {} with addr {} => error {:?}", self.node_id, node_id, slot.addr, err);
-                                    slot.outgoing = OutgoingState::ConnectError(ts_ms, None, err, count + 1);
-                                }
-                            }
+                            self.queue_action.push_back(NetworkBehaviorAction::ConnectTo(0, *node_id, slot.addr.clone()));
+                            slot.outgoing = OutgoingState::Connecting(now_ms, None, count + 1);
                         }
                     }
                     _ => {}
@@ -107,23 +98,39 @@ where
         }
     }
 
-    fn check_incoming_connection(&mut self, _node: NodeId, _conn_id: ConnId) -> Result<(), ConnectionRejectReason> {
+    fn on_awake(&mut self, ctx: &BehaviorContext, now_ms: u64) {}
+
+    fn check_incoming_connection(&mut self, context: &BehaviorContext, now_ms: u64, _node: NodeId, _conn_id: ConnId) -> Result<(), ConnectionRejectReason> {
         Ok(())
     }
 
-    fn check_outgoing_connection(&mut self, _node: NodeId, _conn_id: ConnId) -> Result<(), ConnectionRejectReason> {
-        Ok(())
+    fn check_outgoing_connection(&mut self, context: &BehaviorContext, now_ms: u64, node: NodeId, conn_id: ConnId, local_uuid: TransportOutgoingLocalUuid) -> Result<(), ConnectionRejectReason> {
+        if let Some(neighbour) = self.neighbours.get_mut(&node) {
+            match neighbour.outgoing {
+                OutgoingState::New => {
+                    neighbour.outgoing = OutgoingState::Connecting(now_ms, Some(conn_id), 0);
+                    Ok(())
+                }
+                OutgoingState::Connecting(_, _, count) => {
+                    neighbour.outgoing = OutgoingState::Connecting(now_ms, Some(conn_id), count);
+                    Ok(())
+                }
+                _ => Err(ConnectionRejectReason::ConnectionLimited),
+            }
+        } else {
+            Ok(())
+        }
     }
 
-    fn on_local_event(&mut self, _agent: &BehaviorAgent<BE, HE>, _event: BE) {
+    fn on_local_event(&mut self, _context: &BehaviorContext, now_ms: u64, _event: BE) {
         panic!("Should not happend");
     }
 
-    fn on_local_msg(&mut self, _agent: &BehaviorAgent<BE, HE>, _msg: network::msg::TransportMsg) {
+    fn on_local_msg(&mut self, _context: &BehaviorContext, now_ms: u64, _msg: network::msg::TransportMsg) {
         panic!("Should not happend");
     }
 
-    fn on_incoming_connection_connected(&mut self, _agent: &BehaviorAgent<BE, HE>, conn: Arc<dyn ConnectionSender>) -> Option<Box<dyn ConnectionHandler<BE, HE>>> {
+    fn on_incoming_connection_connected(&mut self, _context: &BehaviorContext, now_ms: u64, conn: Arc<dyn ConnectionSender>) -> Option<Box<dyn ConnectionHandler<BE, HE>>> {
         let entry = self.neighbours.entry(conn.remote_node_id()).or_insert_with(|| NodeSlot {
             addr: conn.remote_addr(),
             incoming: None,
@@ -133,7 +140,13 @@ where
         Some(Box::new(ManualHandler {}))
     }
 
-    fn on_outgoing_connection_connected(&mut self, _agent: &BehaviorAgent<BE, HE>, connection: Arc<dyn ConnectionSender>) -> Option<Box<dyn ConnectionHandler<BE, HE>>> {
+    fn on_outgoing_connection_connected(
+        &mut self,
+        _context: &BehaviorContext,
+        now_ms: u64,
+        connection: Arc<dyn ConnectionSender>,
+        local_uuid: TransportOutgoingLocalUuid,
+    ) -> Option<Box<dyn ConnectionHandler<BE, HE>>> {
         let entry = self.neighbours.entry(connection.remote_node_id()).or_insert_with(|| NodeSlot {
             addr: connection.remote_addr(),
             incoming: None,
@@ -143,29 +156,41 @@ where
         Some(Box::new(ManualHandler {}))
     }
 
-    fn on_incoming_connection_disconnected(&mut self, _agent: &BehaviorAgent<BE, HE>, connection: Arc<dyn ConnectionSender>) {
-        if let Some(slot) = self.neighbours.get_mut(&connection.remote_node_id()) {
+    fn on_incoming_connection_disconnected(&mut self, _context: &BehaviorContext, now_ms: u64, node: NodeId, conn_id: ConnId) {
+        if let Some(slot) = self.neighbours.get_mut(&node) {
             slot.incoming = None;
         }
     }
 
-    fn on_outgoing_connection_disconnected(&mut self, _agent: &BehaviorAgent<BE, HE>, connection: Arc<dyn ConnectionSender>) {
-        if let Some(slot) = self.neighbours.get_mut(&connection.remote_node_id()) {
+    fn on_outgoing_connection_disconnected(&mut self, _context: &BehaviorContext, now_ms: u64, node: NodeId, conn_id: ConnId) {
+        if let Some(slot) = self.neighbours.get_mut(&node) {
             slot.outgoing = OutgoingState::New;
         }
     }
 
-    fn on_outgoing_connection_error(&mut self, _agent: &BehaviorAgent<BE, HE>, node_id: NodeId, conn_id: ConnId, err: &OutgoingConnectionError) {
+    fn on_outgoing_connection_error(
+        &mut self,
+        _context: &BehaviorContext,
+        now_ms: u64,
+        node_id: NodeId,
+        conn_id: Option<ConnId>,
+        local_uuid: TransportOutgoingLocalUuid,
+        err: &OutgoingConnectionError,
+    ) {
         if let Some(slot) = self.neighbours.get_mut(&node_id) {
             if let OutgoingState::Connecting(_, _, count) = slot.outgoing {
-                slot.outgoing = OutgoingState::ConnectError(self.timer.now_ms(), Some(conn_id), err.clone(), count);
+                slot.outgoing = OutgoingState::ConnectError(self.timer.now_ms(), conn_id, err.clone(), count);
             }
         }
     }
 
-    fn on_handler_event(&mut self, _agent: &BehaviorAgent<BE, HE>, _node_id: NodeId, _connection_id: ConnId, _event: BE) {}
+    fn on_handler_event(&mut self, _context: &BehaviorContext, now_ms: u64, _node_id: NodeId, _connection_id: ConnId, _event: BE) {}
 
-    fn on_started(&mut self, _agent: &BehaviorAgent<BE, HE>) {}
+    fn on_started(&mut self, _context: &BehaviorContext, now_ms: u64) {}
 
-    fn on_stopped(&mut self, _agent: &BehaviorAgent<BE, HE>) {}
+    fn on_stopped(&mut self, _context: &BehaviorContext, now_ms: u64) {}
+
+    fn pop_action(&mut self) -> Option<NetworkBehaviorAction<HE>> {
+        self.queue_action.pop_front()
+    }
 }
