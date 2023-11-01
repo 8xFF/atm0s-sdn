5e049d8e5b1d2a3bd653f78698f808456673587c
Giang Minh <giangndm@ows.vn>
2023-11-02 02:43:34 +0700

    fix udp test

--- packages/transports/udp/src/lib.rs
+++ packages/transports/udp/src/lib.rs
@@ -18,10 +18,10 @@ mod tests {
     #[async_std::test]
     async fn simple_network() {
         let node_addr_builder1 = Arc::new(NodeAddrBuilder::default());
-        let tran1 = UdpTransport::new(1, 10001, node_addr_builder1.clone()).await;
+        let tran1 = UdpTransport::new(1, 0, node_addr_builder1.clone()).await;
 
         let node_addr_builder2 = Arc::new(NodeAddrBuilder::default());
-        let tran2 = UdpTransport::new(2, 10002, node_addr_builder2.clone()).await;
+        let tran2 = UdpTransport::new(2, 0, node_addr_builder2.clone()).await;
 
         network::transport_tests::simple::simple_network(tran1, node_addr_builder1.addr(), tran2, node_addr_builder2.addr()).await;
     }
@@ -29,17 +29,17 @@ mod tests {
     #[async_std::test]
     async fn simple_network_connect_addr_not_found() {
         let node_addr_builder1 = Arc::new(NodeAddrBuilder::default());
-        let tran1 = UdpTransport::new(1, 20001, node_addr_builder1.clone()).await;
+        let tran1 = UdpTransport::new(1, 0, node_addr_builder1.clone()).await;
         network::transport_tests::simple::simple_network_connect_addr_not_found(tran1, NodeAddr::from_iter(vec![Protocol::Ip4(Ipv4Addr::new(127, 0, 0, 1)), Protocol::Udp(20002)])).await;
     }
 
     #[async_std::test]
     async fn simple_network_connect_wrong_node() {
         let node_addr_builder1 = Arc::new(NodeAddrBuilder::default());
-        let tran1 = UdpTransport::new(1, 30001, node_addr_builder1.clone()).await;
+        let tran1 = UdpTransport::new(1, 0, node_addr_builder1.clone()).await;
 
         let node_addr_builder2 = Arc::new(NodeAddrBuilder::default());
-        let tran2 = UdpTransport::new(2, 30002, node_addr_builder2.clone()).await;
+        let tran2 = UdpTransport::new(2, 0, node_addr_builder2.clone()).await;
 
         network::transport_tests::simple::simple_network_connect_wrong_node(tran1, node_addr_builder1.addr(), tran2, node_addr_builder2.addr()).await;
     }
