3266e4442f33c09f8199894be62fc3feeb08e782
Giang Minh <giangndm@ows.vn>
2023-11-17 17:17:26 +0700

    Update name and features of runner to atm0s-sdn

--- packages/runner/src/lib.rs
+++ packages/runner/src/lib.rs
@@ -1,25 +1,34 @@
 pub use atm0s_sdn_identity::{NodeAddr, NodeAddrBuilder, NodeId, Protocol};
-pub use atm0s_sdn_key_value::{KeyId, KeySource, KeyValueBehavior, KeyValueBehaviorEvent, KeyValueHandlerEvent, KeyValueMsg, KeyValueSdk, KeyValueSdkEvent, KeyVersion, SubKeyId, ValueType};
-pub use atm0s_sdn_layers_spread_router::SharedRouter;
-pub use atm0s_sdn_layers_spread_router_sync::{LayersSpreadRouterSyncBehavior, LayersSpreadRouterSyncBehaviorEvent, LayersSpreadRouterSyncHandlerEvent};
-pub use atm0s_sdn_manual_discovery::{ManualBehavior, ManualBehaviorConf, ManualBehaviorEvent, ManualHandlerEvent};
 pub use atm0s_sdn_network::plane::{NetworkPlane, NetworkPlaneConfig};
 pub use atm0s_sdn_network::{
     behaviour::{BehaviorContext, ConnectionContext, NetworkBehavior},
     transport::*,
     convert_enum,
 };
-pub use atm0s_sdn_pub_sub::{
-    ChannelIdentify, ChannelUuid, Consumer, ConsumerRaw, ConsumerSingle, Feedback, FeedbackType, LocalPubId, LocalSubId, NumberInfo, Publisher, PublisherRaw, PubsubSdk, PubsubServiceBehaviour,
-    PubsubServiceBehaviourEvent, PubsubServiceHandlerEvent,
-};
-pub use atm0s_sdn_router::RouteRule;
-pub use atm0s_sdn_transport_udp::UdpTransport;
+pub use atm0s_sdn_router::{RouteRule, RouterTable, RouteAction};
 pub use atm0s_sdn_utils::{
     awaker::{Awaker, MockAwaker},
     SystemTimer, Timer,
     error_handle::ErrorUtils,
     option_handle::OptionUtils,
 };
-
 pub use atm0s_sdn_network::msg::*;
+
+#[cfg(feature = "key-value")]
+pub use atm0s_sdn_key_value::{KeyId, KeySource, KeyValueBehavior, KeyValueBehaviorEvent, KeyValueHandlerEvent, KeyValueMsg, KeyValueSdk, KeyValueSdkEvent, KeyVersion, SubKeyId, ValueType};
+#[cfg(feature = "spread-router")]
+pub use atm0s_sdn_layers_spread_router::SharedRouter;
+#[cfg(feature = "spread-router")]
+pub use atm0s_sdn_layers_spread_router_sync::{LayersSpreadRouterSyncBehavior, LayersSpreadRouterSyncBehaviorEvent, LayersSpreadRouterSyncHandlerEvent};
+#[cfg(feature = "manual-discovery")]
+pub use atm0s_sdn_manual_discovery::{ManualBehavior, ManualBehaviorConf, ManualBehaviorEvent, ManualHandlerEvent};
+
+#[cfg(feature = "pub-sub")]
+pub use atm0s_sdn_pub_sub::{
+    ChannelIdentify, ChannelUuid, Consumer, ConsumerRaw, ConsumerSingle, Feedback, FeedbackType, LocalPubId, LocalSubId, NumberInfo, Publisher, PublisherRaw, PubsubSdk, PubsubServiceBehaviour,
+    PubsubServiceBehaviourEvent, PubsubServiceHandlerEvent,
+};
+#[cfg(feature = "transport-udp")]
+pub use atm0s_sdn_transport_udp::UdpTransport;
+#[cfg(feature = "transport-tcp")]
+pub use atm0s_sdn_transport_tcp::TcpTransport;
