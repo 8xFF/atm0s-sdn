b7f9be7f7ff650c7bfececd905406b499edf9985
giangndm <45644921+giangndm@users.noreply.github.com>
2024-07-19 10:15:44 +0700

    chore: update version (#168)

--- bin/deployments/systemd/deploy.sh
+++ bin/deployments/systemd/deploy.sh
@@ -8,7 +8,7 @@ for server in "${!servers[@]}"; do
     if [[ $server == *"_node_id" ]] || [[ $server == *"_name" ]] || [[ $server == *"_public" ]] || [[ $server == *"_ssh_port" ]] || [[ $server == *"_seeds" ]] || [[ $server == *"_collector" ]]; then
         continue
     fi
-    
+
     # Retrieve the node_id and web_addr for the current server
     node_id="${servers["$server"_node_id]}"
     ssh_port="${servers["$server"_ssh_port]:-22}"
@@ -34,17 +34,17 @@ for server in "${!servers[@]}"; do
     fi
 
     echo "/opt/atm0s-sdn-node" >> /tmp/atm0s-sdn-node-sh
-    
+
     echo "Connecting to $server"
-    
+
     # Upload the file
-    ssh -p $ssh_port ${servers[$server]} "rm -f ${servers[$server]}:/opt/atm0s-sdn-node"
+    ssh -p $ssh_port "${servers[$server]}" "rm -f ${servers[$server]}:/opt/atm0s-sdn-node"
     ssh -p $ssh_port "${servers[$server]}" "rm -f ${servers[$server]}:/etc/systemd/system/atm0s-sdn-node.service"
     ssh -p $ssh_port "${servers[$server]}" "rm -f ${servers[$server]}:/opt/atm0s-sdn-node.sh"
     scp -P $ssh_port "../../../target/release/atm0s-sdn-standalone" "${servers[$server]}:/opt/atm0s-sdn-node"
     scp -P $ssh_port "./atm0s-sdn-node.service" "${servers[$server]}:/etc/systemd/system/atm0s-sdn-node.service"
     scp -P $ssh_port "/tmp/atm0s-sdn-node-sh" "${servers[$server]}:/opt/atm0s-sdn-node.sh"
-    
+
     # Execute the command on the server
     ssh -p $ssh_port "${servers[$server]}" "systemctl daemon-reload"
     ssh -p $ssh_port "${servers[$server]}" "systemctl enable atm0s-sdn-node"
